4d0d2a7367e460a73bc340d33f1a48184d011726
Nume Prenume <simpson.entry@gmail.com>
2020-09-10 20:04:53 +0300

    Au fost redactat fisierul,organizat spre lucru din clasa

---
+++ PromovareaTipurilorJ/ExPractic_1.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+public class ExPractic_1{
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int a=rand.nextInt(10);
+		int b=rand.nextInt(10);
+		
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+					System.out.println("Adunarea:");
+					
+					System.out.println("a+b="+(a+b));
+				
+					
+				
+				
+					System.out.println("Diferenta:");
+					
+					System.out.println("a-b="+(a-b));
+				
+				
+					System.out.println("Inmultirea:");
+					
+					System.out.println("a*b="+a*b);
+				
+				
+					System.out.println("Impartirea:");
+					double rs=a/b;
+					System.out.println("a/b="+rs);
+				
+		
+	}
+}
--- Tema2.java
+++ Tema2.java
@@ -1,12 +1,12 @@
-public class Main2{
+public class Tema2{
 	public static void main(String[] args) {
 		byte value = 5;
 		/*1.Compatibil cu int 
 		  2.Intevalul byte contine 5 =(-127 -> +128)  */
 		int x = 100;
-		long value = 300000000L /*Sau -> l */;
+		long valueL = 300000000L /*Sau -> l */;
 		double num = 10.5/*D*/;
-		float fl =12.5F /*Se pune F la sfarsit din cauza 
+		float fl =12.5F ;/*Se pune F la sfarsit din cauza 
 		ca se va socoti double  */
 
 		int sum = 5 + 6 + 8 + 9; 
